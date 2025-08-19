d99317e38015c30be0e6076aa826599fbd1a1910
יהודית טמינו <taminojudit@hotmail.co.il>
2025-08-19 11:06:46 +0300

    starting over

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-
 function App() {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-      <Navbar />
-      <Home />
+     
     </div>
   );
 }
