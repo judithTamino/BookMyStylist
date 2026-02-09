78452051a84cecc00f91e64f5c8038806cabbb68
יהודית טמינו <taminojudit@hotmail.co.il>
2026-02-09 16:23:26 +0200

    add custom error class and refactor error middleware

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,4 +2,4 @@ import { config } from 'dotenv';
 
 config({ path: '.env', quiet: true });
 
-export const { PORT, MONGODB_LOCAL_URI, JWT_SECRET, ADMIN_EMAIL, EMAIL_PASSWORD } = process.env;
+export const {ENV, PORT, MONGODB_LOCAL_URI, JWT_SECRET, ADMIN_EMAIL, EMAIL_PASSWORD } = process.env;
--- backend/src/middleware/error.middleware.js
+++ backend/src/middleware/error.middleware.js
@@ -1,36 +1,87 @@
-const errorHandler = (err, _req, res, next) => {
-  try {
-    let error = { ...err };
-    error.message = err.message;
+import Joi from "joi";
+import { ENV } from "../config/env.js";
+import { CustomError } from "../errors/CustomError.js";
 
-    // Mongoose bad ObjectId
-    if (err.name === "CastError") {
-      error = new Error("Resource not found");
-      error.statusCode = 400;
-    }
+export const errorHandler = (error, _req, res, _next) => {
+  let statusCode = error.statusCode || 500;
+  const isDev = ENV === 'development';
 
-    // Mongoose duplicate key
-    if (err.code === 11000) {
-      error = new Error("Duplicate field value entered");
-      error.statusCode = 400;
-    }
+  const response = {
+    success: false,
+    status: statusCode,
+    message: error.message || 'Something went wrong',
+    errors: error.errors || [],
+    ...(isDev && { stack: error.stack }),
+  };
 
-    // Mongoose validation key
-    if (err.name === "validationError") {
-      const msg = Object.values(err.errors).map(val => val.message);
-      error = new Error(msg.join(", "));
-      error.statusCode = 400;
-    }
-
-    res.status(error.statusCode || 500).json({
-      success: false,
-      msg: error.message || "Something went wrong",
-      stack: process.env.ENV === "development" ? err.stack : null
+  // Joi validation
+  if (Joi.isError(error))
+    return res.status(422).json({
+      ...response,
+      status: 422,
+      message: "Validation error",
+      errors: error.details.map(detail => ({
+        field: detail.context?.key,
+        message: detail.message
+      })),
     });
 
-  } catch (error) {
-    next(error);
-  }
+  // Mongo duplicate key
+  if (error.code === 11000)
+    return res.status(400).json({
+      ...response,
+      message: "Duplicate field value"
+    });
+
+  // CustomError
+  if (error instanceof CustomError)
+    return res.status(error.statusCode).json(response);
+
+  // Default fallback
+  return res.status(statusCode).json(response);
 };
 
-export default errorHandler;
+
+
+
+
+
+
+// const errorHandler = (err, _req, res, next) => {
+
+//   try {
+//     let error = { ...err };
+//     error.message = err.message;
+
+//     // Mongoose bad ObjectId
+//     if (err.name === "CastError") {
+//       error = new Error("Resource not found");
+//       error.statusCode = 400;
+//     }
+
+//     // Mongoose duplicate key
+//     if (err.code === 11000) {
+//       error = new Error("Duplicate field value entered");
+//       error.statusCode = 400;
+//     }
+
+//     // Mongoose validation key
+//     if (err.name === "validationError") {
+//       const msg = Object.values(err.errors).map(val => val.message);
+//       error = new Error(msg.join(", "));
+//       error.statusCode = 400;
+//     }
+
+//     res.status(error.statusCode || 500).json({
+//       success: false,
+//       msg: error.message || "Something went wrong",
+//       stack: process.env.ENV === "development" ? err.stack : null
+//     });
+
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// export default errorHandler;
+
