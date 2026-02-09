9b640e3e82c1729df71193b0557e80c1d374878f
יהודית טמינו <taminojudit@hotmail.co.il>
2026-02-09 17:35:55 +0200

    refactor(auth): use CustomError in auth middleware. feat(auth): move authentication from Bearer header to cookies

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,39 +1,60 @@
 import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
+
 import User from "../models/User.model.js";
 import { JWT_SECRET } from "../config/env.js";
+import { CustomError } from "../errors/CustomError.js";
 
 export const protect = asyncHandler(async (req, _res, next) => {
-  let token = req.headers.authorization;
-
-  if (token && token.startsWith("Bearer"))
-    token = token.split(" ")[1];
-
-  if (!token) {
-    const error = new Error("Please login");
-    error.statusCode = 401;
-    throw error;
-  }
+  const { token } = req.cookies;
+  if (!token)
+    throw new CustomError("Missing or invalid token.", 401);
 
   const decoded = jwt.verify(token, JWT_SECRET);
-
   const user = await User.findById(decoded._id).select("-password");
-  if (!user) {
-    const error = new Error("User not found");
-    error.statusCode = 404;
-    throw error;
-  }
-  
+
+  if (!user)
+    throw new CustomError("User not found.", 404);
+
   req.user = user;
   next();
+
+  // let token = req.headers.authorization;
+
+  // if (token && token.startsWith("Bearer"))
+  //   token = token.split(" ")[1];
+
+  // if (!token) {
+  //   const error = new Error("Please login");
+  //   error.statusCode = 401;
+  //   throw error;
+  // }
+
+  // const decoded = jwt.verify(token, JWT_SECRET);
+
+  // const user = await User.findById(decoded._id).select("-password");
+  // if (!user) {
+  //   const error = new Error("User not found");
+  //   error.statusCode = 404;
+  //   throw error;
+  // }
+
+  // req.user = user;
+  // next();
 });
 
 export const admin = (req, _res, next) => {
-  if (req.user && req.user.isAdmin) next();
-  else {
-    const error = new Error("Access denied, admin only");
-    error.statusCode = 403;
-    throw error;
-  }
+  const user = req.user;
+
+  if(user && user.isAdmin)
+    next();
+  else
+    throw new CustomError("Admin only.", 403)
+  // if (req.user && req.user.isAdmin) next();
+  // else {
+  //   const error = new Error("Access denied, admin only");
+  //   error.statusCode = 403;
+  //   throw error;
+  // }
 };
 
